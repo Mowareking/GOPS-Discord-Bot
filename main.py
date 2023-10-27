43d10e6b9f94bb2c265ce041e55af48863382eb5
Mowareking <88452869+Mowareking@users.noreply.github.com>
2023-10-27 21:43:12 +0100

    added custom card emojis

--- main.py
+++ main.py
@@ -14,10 +14,88 @@ def create_embed(desc, footer=None):
         embed.set_footer(text=footer)
     return embed
 
-def create_card(cards, suit):
-    
-    for card in cards:
+def create_cards(cards, suit):
+    message = ""
 
+    if suit in ["diamonds", "hearts"]:
+        suit_colour = "r"
+    else:
+        suit_colour = "b"
+
+    for card in cards:
+        if suit_colour == "r":
+            match card:
+                case "A":
+                    message += "<:rA:1167547696308027573>"
+                case "2":
+                    message += "<:r2:1167547670118813726>"
+                case "3":
+                    message += "<:r3:1167547671620358175>"
+                case "4":
+                    message += "<:r4:1167547674065649786>"
+                case "5":
+                    message += "<:r5:1167547661075894293>"
+                case "6":
+                    message += "<:r6:1167547664435523674>"
+                case "7":
+                    message += "<:r7:1167547665878368267>"
+                case "8":
+                    message += "<:r8:1167547668323631247>"
+                case "9":
+                    message += "<:r9:1167547707972395018>"
+                case "10":
+                    message += "<:r10:1167547709675286639>"
+                case "J":
+                    message += "<:rJ:1167547700154212402>"
+                case "Q":
+                    message += "<:rQ:1167547704168169607>"
+                case "K":
+                    message += "<:rK:1167547701488009287>"
+
+        if suit_colour == "b":
+            match card:
+                case "A":
+                    message += "<:bA:1167547616259747900>"
+                case "2":
+                    message += "<:b2:1167547529513160824>"
+                case "3":
+                    message += "<:b3:1167547552535691304>"
+                case "4":
+                    message += "<:b4:1167547554515402884>"
+                case "5":
+                    message += "<:b5:1167547556121804920>"
+                case "6":
+                    message += "<:b6:1167547559129137323>"
+                case "7":
+                    message += "<:b7:1167547561935126629>"
+                case "8":
+                    message += "<:b8:1167547564447502456>"
+                case "9":
+                    message += "<:b9:1167547565848416440>"
+                case "10":
+                    message += "<:b10:1167547581140840470>"
+                case "J":
+                    message += "<:bJ:1167547607732723762>"
+                case "Q":
+                    message += "<:bQ:1167546141294993498>"
+                case "K":
+                    message += "<:bK:1167547608974233640>"
+            
+
+    if cards:
+        message += "\n"
+
+    match suit:
+        case "diamonds":
+            message += "<:ediamonds:1167547698711379988>"*len(cards)
+        case "hearts":
+            message += "<:ehearts:1167547614837870592>"*len(cards)
+        case "clubs":
+            message += "<:eclubs:1167547612493262929>"*len(cards)
+        case "spades":
+                message += "<:espades:1167547715270492382>"*len(cards)
+
+    return message
 
 @client.event
 async def on_ready():
@@ -35,7 +113,7 @@ async def on_message(message):
             opponent_author = client.get_user(opponent_id)
             opponent_mention = message.content[6:-1] + ">"
 
-            await message.channel.send(embed=create_embed(f"{message.author.mention} :clubs: wants to play a game of GOPS. Do you accept?\nYes/No"))
+            await message.channel.send(embed=create_embed(f"{message.author.mention} wants to play a game of GOPS with you, {opponent_mention}. Do you accept?\nYes/No"))
             try:
                 opponent_message = await client.wait_for("message", check=lambda reply: reply.content.lower() in ["yes", "no"] and reply.author.id == opponent_id and reply.channel == message.channel, timeout=60.0)
             except asyncio.TimeoutError:
@@ -62,23 +140,29 @@ async def on_message(message):
             random.shuffle(stock)
             upturned = []
 
+            suits = ["diamonds", "hearts", "clubs", "spades"]
+            author_suit = suits.pop()
+            opponent_suit = suits.pop()
+            stock_suit = suits.pop()
+
             for i in range(13):
                 upturned.append(stock.pop())
-                cards_remaining = len(stock)
+                cards_remaining = len(stock)-1
                 author_move, opponent_move = "", ""
                 
                 """
                 await message.channel.send(f"Upturned card(s): {upturned}.\n{message.author.mention}'s prizes: {author_prizes}\n{opponent_mention}'s prizes: {opponent_prizes}\nCards remaining in deck: {cards_remaining}")
                 """
 
-                upturned_display = str(upturned)[1:-1].replace("'", "")
-                author_hand_display = str(author_hand)[1:-1].replace("'", "")
-                opponent_hand_display = str(opponent_hand)[1:-1].replace("'", "")
-                author_prizes_display = str(author_prizes)[1:-1].replace("'", "")
-                opponent_prizes_display = str(opponent_prizes)[1:-1].replace("'", "")
+                upturned_display = create_cards(upturned, stock_suit)
+                print(stock_suit)
+                author_hand_display = create_cards(author_hand, author_suit)
+                opponent_hand_display = create_cards(opponent_hand, opponent_suit)
+                author_prizes_display = create_cards(author_prizes, author_suit)
+                opponent_prizes_display = create_cards(opponent_prizes, opponent_suit)
 
-                await message.author.send(embed=create_embed(f"Upturned card(s):  {upturned_display}\nYour hand:  {author_hand_display}\nYour prizes:  {author_prizes_display}\n{opponent_mention}'s prizes: {opponent_prizes_display}", f"Cards remaining in deck: {cards_remaining}"))
-                await opponent_author.send(embed=create_embed(f"Upturned card(s):  {upturned_display}\nYour hand:  {opponent_hand_display}\nYour prizes:  {opponent_prizes_display}\n{message.author.mention}'s prizes: {author_prizes_display}", f"Cards remaining in deck: {cards_remaining}"))
+                await message.author.send(embed=create_embed(f"Upturned card(s):\n{upturned_display}\nYour hand:\n{author_hand_display}\nYour prizes:\n{author_prizes_display}\n{opponent_mention}'s prizes:\n{opponent_prizes_display}", f"Cards remaining in deck: {cards_remaining}"))
+                await opponent_author.send(embed=create_embed(f"Upturned card(s):\n{upturned_display}\nYour hand:\n{opponent_hand_display}\nYour prizes:\n{opponent_prizes_display}\n{message.author.mention}'s prizes:\n{author_prizes_display}", f"Cards remaining in deck: {cards_remaining}"))
 
                 while not opponent_move or not author_move:
                     try:
@@ -120,8 +204,8 @@ async def on_message(message):
                 
                 
 
-                opponent_hand.remove(opponent_move.content)
-                author_hand.remove(author_move.content)
+                opponent_hand.remove(opponent_move.content.upper())
+                author_hand.remove(author_move.content.upper())
 
                 await message.author.send(embed=create_embed(f"You played a {author_move.content}\n{opponent_mention} played a {opponent_move.content}"))
                 await opponent_author.send(embed=create_embed(f"You played a {opponent_move.content}\n{message.author.mention} played a {author_move.content}"))
